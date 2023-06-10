6538dbb8c49bbe931ac33ddd5edeb554babb8421
jainpranav1 <pranavji@tamu.edu>
2023-06-10 14:00:34 -0500

    added all paths

--- index.ts
+++ index.ts
@@ -29,6 +29,56 @@ router.get("/", async (context) => {
   context.response.body = output;
 });
 
+router.get("/about_me", async (context) => {
+  const output: string = await renderFileToString("views/about_me.ejs", {});
+  context.response.body = output;
+});
+
+router.get("/academics", async (context) => {
+  const output: string = await renderFileToString("views/academics.ejs", {});
+  context.response.body = output;
+});
+
+router.get("/achievements", async (context) => {
+  const output: string = await renderFileToString("views/achievements.ejs", {});
+  context.response.body = output;
+});
+
+router.get("/blockify", async (context) => {
+  const output: string = await renderFileToString("views/blockify.ejs", {});
+  context.response.body = output;
+});
+
+router.get("/cover_up", async (context) => {
+  const output: string = await renderFileToString("views/cover_up.ejs", {});
+  context.response.body = output;
+});
+
+router.get("/git_visualizer", async (context) => {
+  const output: string = await renderFileToString("views/git_visualizer.ejs", {});
+  context.response.body = output;
+});
+
+router.get("/leadership", async (context) => {
+  const output: string = await renderFileToString("views/leadership.ejs", {});
+  context.response.body = output;
+});
+
+router.get("/projects", async (context) => {
+  const output: string = await renderFileToString("views/projects.ejs", {});
+  context.response.body = output;
+});
+
+router.get("/volunteering", async (context) => {
+  const output: string = await renderFileToString("views/volunteering.ejs", {});
+  context.response.body = output;
+});
+
+router.get("/work_experience", async (context) => {
+  const output: string = await renderFileToString("views/work_experience.ejs", {});
+  context.response.body = output;
+});
+
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
